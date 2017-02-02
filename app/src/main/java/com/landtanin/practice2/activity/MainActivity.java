c7dd45a35e7338d693a0daf98683bed74df210b6
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-02-02 21:33:47 +0000

    finished Toolbar

--- app/src/main/java/com/landtanin/practice2/activity/MainActivity.java
+++ app/src/main/java/com/landtanin/practice2/activity/MainActivity.java
@@ -1,17 +1,33 @@
 package com.landtanin.practice2.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.landtanin.practice2.R;
 import com.landtanin.practice2.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout mDrawerLayout;
+
+//    ActivityMainBinding mActivityMainBinding;
+    ActionBarDrawerToggle mActionBarDrawerToggle;
+
+    Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        mActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         setContentView(R.layout.activity_main);
+
+        initInstance();
 
         if (savedInstanceState == null) {
 
@@ -21,5 +37,47 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+    }
+
+    private void initInstance() {
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToolbar = (Toolbar) findViewById(R.id.mainToolbar);
+
+        setSupportActionBar(mToolbar);
+
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                mDrawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer
+        );
+
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        mActionBarDrawerToggle.syncState();
+        super.onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mActionBarDrawerToggle.onConfigurationChanged(newConfig);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mActionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
