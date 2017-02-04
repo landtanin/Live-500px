c62b878b661f2ab12ad0e7bbdc3412cd9abf7509
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-02-04 11:04:46 +0000

    ListView3_multiple_List_Type

--- app/src/main/java/com/landtanin/practice2/adapter/PhotoListAdapter.java
+++ app/src/main/java/com/landtanin/practice2/adapter/PhotoListAdapter.java
@@ -3,6 +3,7 @@ package com.landtanin.practice2.adapter;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.landtanin.practice2.view.PhotoListItem;
 
@@ -26,8 +27,71 @@ public class PhotoListAdapter extends BaseAdapter {
         return 0;
     }
 
+    // --------------multiple ListView type--------------
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position % 2 == 0 ? 0 : 1;
+    }
+
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return new PhotoListItem(viewGroup.getContext());
+
+        if (getItemViewType(i)== 0) {
+
+            PhotoListItem item;
+
+            if (view != null) {
+                item = (PhotoListItem) view;
+
+            }
+            else {
+                item = new PhotoListItem(viewGroup.getContext());
+            }
+
+            return item;
+
+        } else {
+
+            TextView item;
+
+            if (view != null) {
+
+                item = (TextView) view;
+
+            }
+            else {
+                item = new TextView(viewGroup.getContext());
+            }
+
+            item.setText("Position :" + i);
+
+            return item;
+
+        }
+
     }
+
+    // --------------single ListView type--------------
+
+
+//    @Override
+//    public View getView(int i, View view, ViewGroup viewGroup) {
+//        PhotoListItem item;
+//
+//            if (view != null) {
+//                item = (PhotoListItem) view;
+//
+//            }
+//            else {
+//                item = new PhotoListItem(viewGroup.getContext());
+//            }
+//
+//            return item;
+//    }
 }
